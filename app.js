777f6a0e519dfad9f7b13ce98eff6944bba4b409
skytow <acepci@gmail.com>
2016-05-16 18:36:50 -0400

    cleanup localhost app js

--- app.js
+++ app.js
@@ -15,12 +15,6 @@ if ( parsedOptions.proxy ) {
     console.log("Proxying to " + parsedOptions.proxy);
     var restProxy = httpProxy.createProxyServer();
     var options = {target: parsedOptions.proxy, changeOrigin:true};
-    var pathMatch = "/ocx-web/rest/ui/*";
-    app.all(pathMatch, function (req, res) {
-        req.headers[ 'ocxui.development.header'] = 'f8f1ca743dfe7ecbb82c6b26';
-        req.socket.setNoDelay(true);
-        restProxy.web(req, res, options);
-    });
 
     http.createServer(app).listen(app.get('port'), function () {
         console.log("Express Web listening on port: " + app.get('port'));
