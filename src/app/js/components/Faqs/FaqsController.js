0a51f7f3b95fe27ceeda510f5862dc849698cb89
Anthony Vito <anthony.vito@macquarium.com>
2016-07-08 14:51:20 -0400

    remove bootstrap tab initialization from FAQs in preparation for changing it to bootstrap.ui templates.

--- src/app/js/components/Faqs/FaqsController.js
+++ src/app/js/components/Faqs/FaqsController.js
@@ -2,7 +2,7 @@
     function FaqsController($scope, $log, $rootScope){
         var vm = this;
         $rootScope.tutorial = false;
-        $( "#tabs" ).tabs();
+        //$( "#tabs" ).tabs();
     }
     angular.module('emv').controller('FaqsController', FaqsController);
 })();
