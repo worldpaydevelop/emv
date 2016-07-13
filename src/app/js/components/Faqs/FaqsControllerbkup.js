950e1dedb45521428e76d9cefde0af7b5b17d17a
Wesley Chitwood <wesley.chitwood1@gmail.com>
2016-07-12 22:12:34 -0400

    fixing far tabs

---
+++ src/app/js/components/Faqs/FaqsControllerbkup.js
@@ -0,0 +1,8 @@
+(function(){
+    function FaqsController($scope, $log, $rootScope){
+        var vm = this;
+        $rootScope.tutorial = false;
+        //$( "#tabs" ).tabs();
+    }
+    angular.module('emv').controller('FaqsController', FaqsController);
+})();
